8333c2855d008b963f7d71bb3bdfdfee8e2d80b6
gesh <gesh@gesh.uni.cx>
2024-08-28 18:57:12 +0300

    fix tests in view of main changes

--- tests/test_forbidden_flag_combos.py
+++ tests/test_forbidden_flag_combos.py
@@ -1,21 +1,62 @@
-import argparse
+"""
+Test forbidden/supported flag combinations work correctly
+"""
+
 import pytest
-from pip2pkgbuild import main
+import pip2pkgbuild
 
-def test_pep517_py3():
-    main(['pip', '-o', '-p', 'python', '--pep517'])
-def test_pep517_py2():
+
+def main(*args):
+    """
+    Wrapper around pip2pkgbuild.main.
+    It is especially used to set argv[0], but we also set the program to print
+    the PKGBUILD to stdout to prevent side-effects
+    """
+    pip2pkgbuild.main(['pip2pkgbuild', '-o', *args])
+
+
+@pytest.mark.parametrize('python, pep517', [
+    ('python',  '--pep517'),
+    ('python',  '--no-pep517'),
+    ('python2', '--no-pep517'),
+    ('multi',   '--no-pep517')
+    ])
+def test_supported_pep517(python, pep517):
+    """Test supported python/pep517 combinations"""
+    main('pip', '-p', python, pep517)
+
+
+@pytest.mark.parametrize('python, pep517', [
+    ('python2', '--pep517'),
+    ('multi',   '--pep517')
+    ])
+def test_forbidden_pep517(python, pep517):
+    """Test forbidden python/pep517 combinations"""
     with pytest.raises(SystemExit) as e:
-        main(['pip', '-o', '-p', 'python2', '--pep517'])
-    assert e.value.code == 1
-def test_pep517_multi():
-    with pytest.raises(SystemExit) as e:
-        main(['pip', '-o', '-p', 'multi', '--pep517'])
+        main('pip', '-p', python, pep517)
     assert e.value.code == 1
 
-def test_nopep517_py3():
-    main(['pip', '-o', '-p', 'python', '--no-pep517'])
-def test_nopep517_py2():
-    main(['pip', '-o', '-p', 'python2', '--no-pep517'])
-def test_nopep517_multi():
-    main(['pip', '-o', '-p', 'multi', '--no-pep517'])
+
+@pytest.mark.parametrize('python, pydeps', [
+    ('python',  ['--python3-depends', 'pylint']),
+    ('python2', ['--python2-depends', 'pylint']),
+    ('multi',   ['--python2-depends', 'pylint']),
+    ('multi',   ['--python3-depends', 'pylint']),
+    ('multi',   ['--python2-depends', 'pylint', '--python3-depends', 'pylint']),
+    ('multi',   ['--python3-depends', 'pylint', '--python2-depends', 'pylint'])
+    ])
+def test_supported_pydep(python, pydeps):
+    """Test supported python/pydep combinations"""
+    main('pip', '-p', python, *pydeps)
+
+
+@pytest.mark.parametrize('python, pydeps', [
+    ('python',  ['--python2-depends', 'pylint']),
+    ('python',  ['--python2-depends', 'pylint', '--python3-depends', 'pylint']),
+    ('python2', ['--python3-depends', 'pylint']),
+    ('python2', ['--python2-depends', 'pylint', '--python3-depends', 'pylint'])
+    ])
+@pytest.mark.xfail
+def test_forbidden_pydep(python, pydeps):
+    """Test forbidden python/pydep combinations"""
+    main('pip', '-p', python, *pydeps)
